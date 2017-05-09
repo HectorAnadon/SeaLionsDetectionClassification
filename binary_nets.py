a09fa4f5794c303d937ea78c30c3520e3a98db02
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2017-05-09 17:27:28 +0200

    change to layer by layer implementation

--- binary_nets.py
+++ binary_nets.py
@@ -1,7 +1,8 @@
 import numpy as np
 np.random.seed(1337)  # for reproducibility
 
-from keras.models import Sequential
+from keras.models import Sequential, Model
+from keras.layers import Input
 from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.merge import concatenate
@@ -50,18 +51,51 @@ def build_12_net():
     
 #     return model
 
+def build_50_net():
+    input_img_50 = Input(shape=(50, 50, 3))
+
+    conv2d_50 = Conv2D(64, (5, 5), input_shape=input_img_50.shape, strides=1, padding='same', dilation_rate=1, 
+        activation='relu', use_bias=True, kernel_initializer='glorot_uniform', 
+        bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
+        activity_regularizer=None, kernel_constraint=None, bias_constraint=None) (input_img_50)
+
+    max_pooling2d_50 = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid')(conv2d_50)
+
+    flatten_50 = Flatten()(max_pooling2d_50)
+
+
+    input_img_25 = Input(shape=(25, 25, 3))
+    conv2d_25 = Conv2D(16, (5, 5), input_shape=input_img_25.shape, strides=1, padding='same', dilation_rate=1, 
+        activation='relu', use_bias=True, kernel_initializer='glorot_uniform', 
+        bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
+        activity_regularizer=None, kernel_constraint=None, bias_constraint=None) (input_img_25)
+    max_pooling2d_25 = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid')(conv2d_25)
+    flatten_25 = Flatten()(max_pooling2d_25)
+
+
+    flatten_merged = concatenate([flatten_50, flatten_25])
+
+    dense = Dense(2, activation='relu', use_bias=True, kernel_initializer='glorot_uniform', 
+        bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
+        activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(flatten_merged)
+
+    model = Model(inputs = [input_img_50 , input_img_25] , outputs = dense)
+
+    return model
+
+
 
 """Testing"""
 
-print "\n\t12_net\n"
-m12 = build_12_net()
-print m12.summary()
-print "FC layer:\n", m12.layers[-1], m12.layers[-1].get_config()
+# print "\n\t12_net\n"
+# m12 = build_12_net()
+# print m12.summary()
+# print "FC layer:\n", m12.layers[-1], m12.layers[-1].get_config()
 
-# print "\n\t24_net\n"
-# m24 = build_24_net()
-# print m24.summary()
-# print "FC layer:\n", m24.layers[-1], m24.layers[-1].get_config()
+print "\n\t50_net\n"
+m50 = build_50_net()
+print m50.summary()
+print "FC layer:\n", m50.layers[-1], m50.layers[-1].get_config()
 
 
 
