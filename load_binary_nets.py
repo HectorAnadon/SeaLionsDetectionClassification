2407fa88e473d83b91df053abb448d6fcc3c780b
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2017-05-14 12:40:19 +0200

    add working model checkpointer, save and load weights

--- load_binary_nets.py
+++ load_binary_nets.py
@@ -1,13 +1,10 @@
 import pdb
 import sys
 from keras.utils.io_utils import HDF5Matrix
-from keras.callbacks import ModelCheckpoint
-from PIL import Image
-import matplotlib.pyplot as plt
 from binary_nets import *
 
 
-def train_net1():
+def load_net1():
 	# Instante HDF5Matrix for the training set
 	X_train = HDF5Matrix('data_net1_small.h5', 'data', start=0, end=250)
 	y_train = HDF5Matrix('data_net1_small.h5', 'labels', start=0, end=250)
@@ -25,23 +22,11 @@ def train_net1():
 	layer, model = build_net_1(Input(shape=(X_train.shape[1], X_train.shape[2], 3)))
 	print model.summary()
 
+	# Load weights
+	model.load_weights('weights_net1_best.hdf5')
+
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
-
- 	# Checkpoint (for saving the weights)
-	filepath = 'weights_net1_best.hdf5'
-	checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, 
-			save_weights_only=True, mode='max')
-	callbacks_list = [checkpoint]
-	
-	# Train model (and save the weights)
-	model.fit(X_train, y_train,
-			batch_size=32,
-			epochs=30,
-			validation_data=(X_test, y_test),
-			shuffle='batch', # Have to use shuffle='batch' or False with HDF5Matrix
-			verbose=0, 
-			callbacks=callbacks_list)
 
 	# Evaluate model
 	scores = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
@@ -49,11 +34,11 @@ def train_net1():
 	print("%s:\t%.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 
-def train_net2():
+def load_net2():
 	pass
 
 
-def train_net3():
+def load_net3():
 	pass
 
 
@@ -61,14 +46,14 @@ if __name__ == '__main__':
     try:
         arg1 = sys.argv[1]
     except IndexError:
-        print("Command line argument missing. Usage: train_binary_nets.py <net number>")
+        print("Command line argument missing. Usage: load_binary_nets.py <net number>")
         sys.exit(1)
     if arg1 == '1':
-    	train_net1()
+    	load_net1()
     elif arg1 == '2':
-    	train_net2()
+    	load_net2()
     elif arg1 == '3':
-    	train_net3()
+    	load_net3()
     else:
     	print("Wrong command line argument. Must be a value between 1-3.")
 
