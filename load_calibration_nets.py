0aee867a830ee5b2f8ccca92abb474619898704f
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2017-05-16 16:29:09 +0200

    tarining for calib 2 and 3, load and test all calibration nets

---
+++ load_calibration_nets.py
@@ -0,0 +1,99 @@
+import pdb
+import sys
+from keras.utils.io_utils import HDF5Matrix
+from calibration_nets import *
+from make_datasets import *
+
+
+def load_calibr_net1():
+	""" Load calibr_net1 model and check validation accuracy (should be same as the best
+		validation accuracy when the model was trained).
+	"""
+	# Load data
+	X_data = HDF5Matrix('Datasets/data_calib1_small.h5', 'data')
+	y_data = HDF5Matrix('Datasets/data_calib1_small.h5', 'labels')
+	# Split into training and validation sets (same used during training)
+	X_train, y_train, X_test, y_test = split_data(X_data, y_data, TRAIN_SPLIT)
+	# Zero center
+	means = np.load('Datasets/means_calib1.npy')
+	X_test -= means
+	# Create model
+	model = calibration_net_1(Input(shape=(X_train.shape[1], X_train.shape[2], 3)), N_CALIBRATION_TRANSFORMATIONS)
+	print(model.summary())
+	# Load weights
+	model.load_weights('Weights/weights_calibration_net1.hdf5')
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+	# Evaluate model (Check validation accuracy)
+	scores = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
+	print("%s:\t%.4f" % (model.metrics_names[0], scores[0]))
+	print("%s:\t%.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+def load_calibr_net2():
+	""" Load calibr_net2 model and check validation accuracy (should be same as the best
+		validation accuracy when the model was trained).
+	"""
+	# Load data
+	X_data = HDF5Matrix('Datasets/data_calib2_small.h5', 'data')
+	y_data = HDF5Matrix('Datasets/data_calib2_small.h5', 'labels')
+	# Split into training and validation sets (same used during training)
+	X_train, y_train, X_test, y_test = split_data(X_data, y_data, TRAIN_SPLIT)
+	# Zero center
+	means = np.load('Datasets/means_calib2.npy')
+	X_test -= means
+	# Create model
+	model = calibration_net_2(Input(shape=(X_train.shape[1], X_train.shape[2], 3)), N_CALIBRATION_TRANSFORMATIONS)
+	print(model.summary())
+	# Load weights
+	model.load_weights('Weights/weights_calibration_net2.hdf5')
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+	# Evaluate model (Check validation accuracy)
+	scores = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
+	print("%s:\t%.4f" % (model.metrics_names[0], scores[0]))
+	print("%s:\t%.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+def load_calibr_net3():
+	""" Load calibr_net3 model and check validation accuracy (should be same as the best
+		validation accuracy when the model was trained).
+	"""
+	# Load data
+	X_data = HDF5Matrix('Datasets/data_calib3_small.h5', 'data')
+	y_data = HDF5Matrix('Datasets/data_calib3_small.h5', 'labels')
+	# Split into training and validation sets (same used during training)
+	X_train, y_train, X_test, y_test = split_data(X_data, y_data, TRAIN_SPLIT)
+	# Zero center
+	means = np.load('Datasets/means_calib3.npy')
+	X_test -= means
+	# Create model
+	model = calibration_net_3(Input(shape=(X_train.shape[1], X_train.shape[2], 3)), N_CALIBRATION_TRANSFORMATIONS)
+	print(model.summary())
+	# Load weights
+	model.load_weights('Weights/weights_calibration_net3.hdf5')
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+	# Evaluate model (Check validation accuracy)
+	scores = model.evaluate(X_test, y_test, batch_size=32, verbose=0)
+	print("%s:\t%.4f" % (model.metrics_names[0], scores[0]))
+	print("%s:\t%.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
+"""Testing"""
+if __name__ == '__main__':
+    try:
+        arg1 = sys.argv[1]
+    except IndexError:
+        print("Command line argument missing. Usage: load_calibration_nets.py <net number>")
+        sys.exit(1)
+    if arg1 == '1':
+    	load_calibr_net1()
+    elif arg1 == '2':
+    	load_calibr_net2()
+    elif arg1 == '3':
+    	load_calibr_net3()
+    else:
+    	print("Wrong command line argument. Must be a value between 1-3.")
+
+
--- train_calibr_nets.py
+++ train_calibr_nets.py
@@ -1,53 +1,129 @@
-import pdb
+import sys, pdb
 from keras.utils.io_utils import HDF5Matrix
 from keras.callbacks import ModelCheckpoint
 from PIL import Image
 import matplotlib.pyplot as plt
 from calibration_nets import *
-from sklearn import model_selection as ms
 from global_variables import *
+from make_datasets import *
 
 def train_calibr_net1():
-    X_data = HDF5Matrix('Datasets/data_callib1_small.h5', 'data')
-    y_data = HDF5Matrix('Datasets/data_callib1_small.h5', 'labels')
-    print (X_data.shape)
-    print (y_data.shape)
-
-    num_images = y_data.shape[0]
-    train_split = TRAIN_SPLIT
-    X_train = X_data[0:int(round(train_split*num_images))]
-    y_train = y_data[0:int(round(train_split*num_images))]
-    X_test = X_data[int(round(train_split*num_images))+1:-1]
-    y_test = y_data[int(round(train_split*num_images))+1:-1]
+    """Train the first binary net and save training data means and best model weights.
+    """
+    # Load data
+    X_data = HDF5Matrix('Datasets/data_calib1_small.h5', 'data')
+    y_data = HDF5Matrix('Datasets/data_calib1_small.h5', 'labels')
+    # Split into training and validation sets
+    X_train, y_train, X_test, y_test = split_data(X_data, y_data, TRAIN_SPLIT)
     # Zero center
     means = np.mean(X_train, axis = 0)
     X_train -= means
     X_test -= means
-
+    # Save means (for testing)
     np.save('Datasets/means_calib1.npy',means)
-
+    # Create model
     model = calibration_net_1(Input(shape=(X_train.shape[1], X_train.shape[2], 3)), N_CALIBRATION_TRANSFORMATIONS)
-
+    # Compile model
     model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
-
-    # print (model.summary())
-    # Note: you have to use shuffle='batch' or False with HDF5Matrix
-    #model.fit(X_train, y_train, batch_size=32, shuffle='batch')
-
+    print(model.summary())
+    # Checkpoint (for saving the weights)
     filepath = 'Weights/weights_calibration_net1.hdf5'
     checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, 
             save_weights_only=True, mode='max')
     callbacks_list = [checkpoint]
-
+    # Train model (and save the weights)
     model.fit(X_train, y_train,
 	          batch_size=32,
 	          epochs=30,
 	          verbose=1,
 	          validation_data=(X_test, y_test),
-	          shuffle='batch', 
+	          shuffle='batch', # have to use shuffle='batch' or False with HDF5Matrix
               callbacks=callbacks_list)
 
-    model.evaluate(X_test, y_test, batch_size=32)
 
+def train_calibr_net2():
+    """Train the second binary net and save training data means and best model weights.
+    """
+    # Load data
+    X_data = HDF5Matrix('Datasets/data_calib2_small.h5', 'data')
+    y_data = HDF5Matrix('Datasets/data_calib2_small.h5', 'labels')
+    # Split into training and validation sets
+    X_train, y_train, X_test, y_test = split_data(X_data, y_data, TRAIN_SPLIT)
+    # Zero center
+    means = np.mean(X_train, axis = 0)
+    X_train -= means
+    X_test -= means
+    # Save means (for testing)
+    np.save('Datasets/means_calib2.npy',means)
+    # Create model
+    model = calibration_net_2(Input(shape=(X_train.shape[1], X_train.shape[2], 3)), N_CALIBRATION_TRANSFORMATIONS)
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    print(model.summary())
+    # Checkpoint (for saving the weights)
+    filepath = 'Weights/weights_calibration_net2.hdf5'
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, 
+            save_weights_only=True, mode='max')
+    callbacks_list = [checkpoint]
+    # Train model (and save the weights)
+    model.fit(X_train, y_train,
+              batch_size=32,
+              epochs=30,
+              verbose=1,
+              validation_data=(X_test, y_test),
+              shuffle='batch', # have to use shuffle='batch' or False with HDF5Matrix
+              callbacks=callbacks_list)
+
+
+def train_calibr_net3():
+    """Train the third binary net and save training data means and best model weights.
+    """
+    # Load data
+    X_data = HDF5Matrix('Datasets/data_calib3_small.h5', 'data')
+    y_data = HDF5Matrix('Datasets/data_calib3_small.h5', 'labels')
+    # Split into training and validation sets
+    X_train, y_train, X_test, y_test = split_data(X_data, y_data, TRAIN_SPLIT)
+    # Zero center
+    means = np.mean(X_train, axis = 0)
+    X_train -= means
+    X_test -= means
+    # Save means (for testing)
+    np.save('Datasets/means_calib3.npy',means)
+    # Create model
+    model = calibration_net_3(Input(shape=(X_train.shape[1], X_train.shape[2], 3)), N_CALIBRATION_TRANSFORMATIONS)
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='Adam', metrics=['accuracy'])
+    print(model.summary())
+    # Checkpoint (for saving the weights)
+    filepath = 'Weights/weights_calibration_net3.hdf5'
+    checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, 
+            save_weights_only=True, mode='max')
+    callbacks_list = [checkpoint]
+    # Train model (and save the weights)
+    model.fit(X_train, y_train,
+              batch_size=32,
+              epochs=30,
+              verbose=1,
+              validation_data=(X_test, y_test),
+              shuffle='batch', # have to use shuffle='batch' or False with HDF5Matrix
+              callbacks=callbacks_list)
+
+
+"""Testing"""
 if __name__ == '__main__':
-    train_calibr_net1()
+    try:
+        arg1 = sys.argv[1]
+    except IndexError:
+        print("Command line argument missing. Usage: train_calibr_nets.py <net number>")
+        sys.exit(1)
+    if arg1 == '1':
+        train_calibr_net1()
+    elif arg1 == '2':
+        train_calibr_net2()
+    elif arg1 == '3':
+        train_calibr_net3()
+    else:
+        print("Wrong command line argument. Must be a value between 1-3.")
+
+
+
