217de06f63606b7feea56106227dcbed63b576b8
albertbou <albertbou92@gmail.com>
2017-05-17 17:04:09 +0200

    Add batchnorm +  predict calibrations net 3

--- calibration_nets.py
+++ calibration_nets.py
@@ -10,6 +10,7 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.layers.merge import concatenate
 from keras import regularizers
 from keras.utils import np_utils
+from keras.layers.normalization import BatchNormalization
 
 def calibration_net_1(input_img, N):
 	conv2d = Conv2D(16, (3, 3), input_shape=input_img.shape, strides=1, padding='same', dilation_rate=1, 
@@ -47,12 +48,14 @@ def calibration_net_3(input_img, N):
         activation='relu', use_bias=True, kernel_initializer='glorot_uniform', 
         bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
         activity_regularizer=None, kernel_constraint=None, bias_constraint=None) (input_img)
-	max_pooling2d = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid')(conv2d_1)
+	batch_norm_1 = BatchNormalization()(conv2d_1)
+	max_pooling2d = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid')(batch_norm_1)
 	conv2d_2 = Conv2D(64, (5, 5), strides=1, padding='same', dilation_rate=1, 
         activation='relu', use_bias=True, kernel_initializer='glorot_uniform', 
         bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
         activity_regularizer=None, kernel_constraint=None, bias_constraint=None) (max_pooling2d)
-	flatten = Flatten()(conv2d_2)
+	batch_norm_2 = BatchNormalization()(conv2d_2)
+	flatten = Flatten()(batch_norm_2)
 	dense1 = Dense(256, activation='relu', use_bias=True, kernel_initializer='glorot_uniform', 
         bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
         activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(flatten)
@@ -60,7 +63,6 @@ def calibration_net_3(input_img, N):
         bias_initializer='zeros', kernel_regularizer=None, bias_regularizer=None, 
         activity_regularizer=None, kernel_constraint=None, bias_constraint=None)(dense1)
 	return Model(inputs=input_img, outputs=dense2)
-
 
 """Testing"""
 if __name__ == "__main__":
@@ -79,5 +81,5 @@ if __name__ == "__main__":
 	input_size = 100
 	input_img = Input(shape=(input_size, input_size, 3))
 	print ("\nnet_3\n")
-	m_3 = calibration_net_3(input_img)
+	m_3 = calibration_net_3(input_img,9)
 	print (m_3.summary())
