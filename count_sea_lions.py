a167813268553bd825294999006f3231ffd3ace7
Beatrice Ionascu <beatrice.ionascu@nyu.edu>
2017-05-22 12:54:02 +0200

    statistics per class

--- count_sea_lions.py
+++ count_sea_lions.py
@@ -10,9 +10,10 @@ def count_sea_lions(image_name, image):
 	corners = np.load(PATH + 'Results/corners_net3_' + image_name + '.npy')
 	windows = getWindows(corners, image, 1)
 	prediction = predict_classification_net(windows, image_name)
-	counts = Counter(prediction)
+	counts = Counter(prediction) 
 	np.save(PATH + 'Results/count_'+ image_name + '.npy', counts)
 	print(image_name, counts)
+	return corners, prediction, counts
 
 
 def count_folder(path):
@@ -20,7 +21,76 @@ def count_folder(path):
 	for image_name in file_names:
 		if image_name.endswith('.jpg'):
 			image = Image.open(PATH + path + image_name)
-			count_sea_lions(image_name, image)
+			counts = count_sea_lions(image_name, image)
+
+
+
+
+def evaluate_result(path, pathDotted, visualize=False):
+	file_names = os.listdir(PATH + path)
+	avg_recall = [0.0]*5
+	avg_precision = [0.0]*5
+	num_files = [0.0]*5
+
+	for image_name in file_names:
+		if image_name.endswith('.jpg'):
+			for i in range(len(num_files)):
+				num_files[i] += 1
+			image = Image.open(PATH + path + image_name)
+			imageDotted = Image.open(PATH + pathDotted + image_name)
+
+			parent_folder = path.split("/")
+			parent = ""
+			for i in range(len(parent_folder)-2):
+				parent += parent_folder[i]
+				parent += "/"
+			coordinates = extractCoordinates(PATH, image_name, parent)
+
+			corners, prediction, counts = count_sea_lions(image_name, image)
+
+			classes = enumerate(CLASSES)
+            for class_index, lion_class in classes:
+				precision = 0.0 # sealions in our res / total res
+				recall = 0.0 # sealions in our res / total sealions
+				count = 0.0
+				total_dots = 0.0
+				for coordinate in coordinates[lion_class][image_name]:
+					#print(coordinate[0]-ORIGINAL_WINDOW_DIM/2, coordinate[1]-ORIGINAL_WINDOW_DIM/2)
+					total_dots += 1
+					for idx in range(len(corners)):
+						if prediction[idx] == class_index and abs(corner[0] - (coordinate[0]-ORIGINAL_WINDOW_DIM/2)) < EVALUATION_MARGIN and \
+								abs(corner[1] - (coordinate[1]-ORIGINAL_WINDOW_DIM/2)) < EVALUATION_MARGIN:
+							count += 1
+							if visualize:
+								print("MATCH: ", corner)
+								plt.imshow(cropAndChangeResolution(imageDotted,image_name,corner[0],corner[1],ORIGINAL_WINDOW_DIM,1))
+								plt.show()
+								plt.imshow(cropAndChangeResolution(image,image_name,corner[0],corner[1],ORIGINAL_WINDOW_DIM,1))
+								plt.show()
+							break;
+					if visualize:
+						plt.imshow(cropAndChangeResolution(imageDotted,image_name,coordinate[0]-ORIGINAL_WINDOW_DIM/2,coordinate[1]-ORIGINAL_WINDOW_DIM/2,ORIGINAL_WINDOW_DIM,1))
+						plt.show()
+				try: 
+					recall = count / total_dots
+					precision = count / len(counts[class_index])
+					print("recall: ", recall)
+					print("precision: ", precision)
+					avg_recall[class_index] += recall
+					avg_precision[class_index] += precision
+				except:
+					print("Skipping image ", image_name, " as it contains no sealions.")
+					num_files[class_index] -= 1
+
+	for idx in range(len(avg_recall)):
+		avg_recall[idx] /= num_files[idx]
+		avg_precision[idx] /= num_files[idx]
+
+	print("AVG recall: ", avg_recall)
+	print("AVG precision: ", avg_precision)
+
+
+
         	
 
 """Testing"""
@@ -32,4 +102,12 @@ if __name__ == '__main__':
 		print("Command line argument missing. Usage: count_sea_lions.py path_to_folder/ (Test/)")
 		sys.exit(1)
 
-	count_folder(arg1)
+	if (len(sys.argv)==4):
+		evaluate_result(arg1, sys.argv[2], visualize=True)
+	elif (len(sys.argv)==3):
+		evaluate_result(arg1, sys.argv[2])
+	else:
+		count_folder(arg1)
+
+
+
